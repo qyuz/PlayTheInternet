586a6731a2e0e6131421d81d6020bcd2eb8e50d7
ab <qyuz555@gmail.com>
2015-04-21 18:46:53 +0300

    Panel Player, Playing, Playlists(Kill/reinit popup player when new one is playing)

--- kladecytI/web/js/lib/player/iframe-observer.js
+++ kladecytI/web/js/lib/player/iframe-observer.js
@@ -1,5 +1,5 @@
 define(["player/pti-abstract", "player/iframe-wrapper", "jquery", "underscore", "jstorage"], function (PTI, IframeWrapper, c, d, e) {
-    var iframeContainer, iframeObserver, initAndListenThrottle, iw, pti;
+    var iframeContainer, iframeObserver, initAndListenThrottle, iw, pti, state;
     var host = "0-71.playtheinternet.appspot.com"
 //        var host = "playtheinternet.appspot.com"
 //        var host = "web.playtheinter.net"
@@ -7,7 +7,7 @@ define(["player/pti-abstract", "player/iframe-wrapper", "jquery", "underscore", 
     var observerReady = new $.Deferred(), youtubeReady, soundcloudReady
 
     iframeContainer = $('#players')
-    pti = initPTI()
+    pti = _definePTI()
     initAndListenThrottle = _.throttle(initAndListen, initTimeout, { trailing: false })
 
     iframeObserver = {
@@ -20,67 +20,18 @@ define(["player/pti-abstract", "player/iframe-wrapper", "jquery", "underscore", 
 
     return iframeObserver;
 
-    function _events() {
-        $('#playersContainer').on('click', '#parsedError', function() {
-            _state('playing');
-            lazyLoadVideo();
-        })
-    }
-
-    function _state(state) {
-        $('#playersContainer').toggleClass('destroyed', state == 'destroyed');
-        $('#playersContainer').toggleClass('playing', state == 'playing');
-        $('#playersContainer').toggleClass('loading', state == 'loading');
-    }
-
-    function appendIframe() {
+    function _appendIframe() {
         var playerIframe
 
         youtubeReady = new $.Deferred()
         soundcloudReady = new $.Deferred()
-
         iframeContainer.html('<iframe class="leftFull temp-border-none temp-width-hundred-percent" src="http://' + host + '/iframe-player.html?origin=' + window.location.href + '"></iframe>')
         playerIframe = iframeContainer.find('iframe').get(0).contentWindow
 
         return playerIframe
     }
 
-    function destroy() {
-        observerReady = new $.Deferred();
-        lastReady = 0;
-
-        _state('destroyed');
-        removeIframe();
-    }
-
-    function initAndListen() {
-        init()
-        clearTimeout(initFailTimeout)
-        var initFailTimeout = setTimeout(function () {
-            console.log('failed to init players in observer, retrying')
-            initAndListenThrottle()
-        }, initTimeout + 500)
-        $.when(youtubeReady, soundcloudReady).then(function () {
-            clearTimeout(initFailTimeout)
-            pti.playing(pti.playing())
-            var volume = $.jStorage.get('volume')
-            pti.volume(volume)
-            var loadVideo = pti.loadVideo()
-            iw.postMessage('pti', 'loadVideo', loadVideo[0], loadVideo[1], loadVideo[2])
-            lastReady = Date.now()
-            observerReady.resolve()
-        })
-    }
-
-    function lazyLoadVideo(thistype, thisoperation, type, videoId, playerState) {
-        if (Date.now() - lastReady >= reinitInterval) {
-            initAndListenThrottle()
-        } else {
-            iw.postMessage(thistype, thisoperation, type, videoId, playerState)
-        }
-    }
-
-    function initPTI() {
+    function _definePTI() {
         var pti = new PTI({
             onLoadVideo: function (type, videoId, playerState) {
                 !_.isUndefined(type) && !_.isUndefined(videoId) && lazyLoadVideo(this.type, this.operation, type, videoId, playerState)
@@ -172,63 +123,113 @@ define(["player/pti-abstract", "player/iframe-wrapper", "jquery", "underscore", 
         return pti
     }
 
-    function init() {
-        if(observerReady.state() == "pending") {
-			_events();
-            appendIframe()
-            iw = initIframeWrapper()
-            initAndListenThrottle()
-        } else {
-            reinit()
-        }
-        return observerReady
+    function _events() {
+        window.addEventListener("unload", function () {
+            if(_state() == 'loading' || _state() == 'playing') {
+                window.chrome.extension.getBackgroundPage().ptiManager.startBackgroundPlayer();
+            }
+        }, true);
+        $('#playersContainer').on('click', '#parsedError', function() {
+            _state('playing');
+            startPlayer();
+        })
     }
 
-    function initIframeWrapper() {
-        var playerIframe, iw
+    function _defineIframeWrapper(playerIframe) {
+        var iw
 
-        playerIframe = appendIframe()
         iw = new IframeWrapper(playerIframe, ["http://" + host])
-
         iw.listenAllEvents(pti.players)
 
         return iw;
     }
 
-    function removeIframe() {
+    function _state(_state) {
+        if(arguments.length) {
+            state = _state;
+            $('#playersContainer').toggleClass('destroyed', _state == 'destroyed');
+            $('#playersContainer').toggleClass('playing', _state == 'playing');
+            $('#playersContainer').toggleClass('loading', _state == 'loading');
+        }
+
+        return state;
+    }
+
+    function _removeIframe() {
         iframeContainer.find('iframe').attr('src', null)
         iframeContainer.empty()
         iw.iframe = null
     }
 
+
+    function destroy() {
+        observerReady = new $.Deferred();
+        youtubeReady.reject();
+        soundcloudReady.reject();
+        lastReady = 0;
+
+        _state('destroyed');
+        _removeIframe();
+    }
+
+    function initAndListen() {
+        clearTimeout(initFailTimeout)
+        var initFailTimeout = setTimeout(function () {
+            console.log('failed to init players in observer, retrying')
+            initAndListenThrottle()
+        }, initTimeout + 500)
+        $.when(youtubeReady, soundcloudReady).then(function () {
+            clearTimeout(initFailTimeout)
+            pti.playing(pti.playing())
+            var volume = $.jStorage.get('volume')
+            pti.volume(volume)
+            var loadVideo = pti.loadVideo()
+            iw.postMessage('pti', 'loadVideo', loadVideo[0], loadVideo[1], loadVideo[2])
+            lastReady = Date.now()
+            observerReady.resolve()
+        })
+    }
+
+    function lazyLoadVideo(thistype, thisoperation, type, videoId, playerState) {
+        if (Date.now() - lastReady >= reinitInterval) {
+            initAndListenThrottle()
+        } else {
+            iw.postMessage(thistype, thisoperation, type, videoId, playerState)
+        }
+    }
+
+    function init() {
+        var playerIframe;
+
+        if(_state() == undefined) {
+			_events();
+            playerIframe = _appendIframe();
+            iw = _defineIframeWrapper(playerIframe);
+            initAndListenThrottle()
+        } else {
+            reinit()
+        }
+
+        return observerReady
+    }
+
     function reinit() {
         var playerIframe
 
-        removeIframe()
-        playerIframe = appendIframe()
+        _removeIframe()
+        playerIframe = _appendIframe()
         iw.iframe = playerIframe
     }
 
     function startPlayer() {
-        var backgroundWindow, $spinner;
-
-        backgroundWindow = window.chrome.extension.getBackgroundPage();
-
-        window.addEventListener("unload", function () {
-            backgroundWindow.ptiManager.startBackgroundPlayer();
-        }, true);
         window.observer = iframeObserver; //maybe remove this
         iframeObserver.init().then(function () {
             window.pti = pti;
             window.playerWidget.data.listenObject = window.pti;
 
-            backgroundWindow.ptiManager.playingWindow(window);
-
-            $spinner = $('#spinner-container');
-            $spinner.animate({ opacity: 0 }, { duration: 1500, complete: function() {
-                _state('playing');
-                $spinner.remove();
-            }});
+            window.chrome.extension.getBackgroundPage().ptiManager.playingWindow(window);
+            _state('playing');
         })
+        _state('loading');
     }
 })
