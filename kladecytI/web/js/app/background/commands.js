50be26752cad0c7774efc79986b6c91b30d6018c
ab <qyuz555@gmail.com>
2015-05-18 19:26:24 +0300

    Skip forward/backward commands

--- kladecytI/web/js/app/background/commands.js
+++ kladecytI/web/js/app/background/commands.js
@@ -1,28 +1,61 @@
+'use strict';
+
 define(["underscore"], function() {
-    var actions = {
+    var actions, backgroundWindow;
+
+    backgroundWindow = window;
+
+    actions = {
         "play": function() {
-            var pti = _.getPti()
+            var pti = _.getPti();
             pti.playVideo()
         },
         "pause": function() {
-            var pti = _.getPti()
+            var pti = _.getPti();
             pti.pauseVideo()
         },
         "next": function() {
-            playlist.playVideo({videoDiv: playlist.lookupNextSong()})
+            playlist.playVideo({videoDiv: playlist.lookupNextSong()});
         },
         "prev": function() {
-            playlist.playVideo({videoDiv: playlist.lookupPrevSong()})
+            playlist.playVideo({videoDiv: playlist.lookupPrevSong()});
         },
         "play/pause": function() {
-            var pti = _.getPti()
-            pti.playing() ? pti.pauseVideo() : pti.playVideo()
+            var pti = _.getPti();
+            pti.playing() ? pti.pauseVideo() : pti.playVideo();
         },
         "panel": function() {
-            _.openPanel()
-        }
-    }
+            _.openPanel();
+        },
+        "skipForward": _.partial(skip, "forward"),
+        "skipBackward": _.partial(skip, "backward")
+    };
     chrome.commands.onCommand.addListener(function(command) {
         actions[command]()
-    })
-})
+    });
+
+    function skip(direction) {
+        var pti, calculatedTime, currentTime, duration, temp;
+
+        pti = backgroundWindow.ptiManager.pti;
+        if (pti) {
+            temp = pti.get(['currentTime', 'duration']);
+            currentTime = temp[0];
+            duration = temp[1];
+            if (_.isNumber(currentTime) && _.isNumber(duration)) {
+                if (direction == "forward") {
+                    calculatedTime = currentTime + 180;
+                    if (calculatedTime >= duration) {
+                        calculatedTime = duration - 1;
+                    }
+                } else {
+                    calculatedTime = currentTime - 180;
+                    if (calculatedTime < 0) {
+                        calculatedTime = 0.01;
+                    }
+                }
+                pti.seekTo(calculatedTime);
+            }
+        }
+    }
+});
